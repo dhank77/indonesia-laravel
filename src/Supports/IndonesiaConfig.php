e27a70ba95668a8f2dd5ceefddabe61979f33f4b
SyahrulBhudiF <syahrul4w@gmail.com>
2025-06-21 06:55:02 +0700

    chore: change readonly to att cause php 8.1 not support class readonly

--- src/Supports/IndonesiaConfig.php
+++ src/Supports/IndonesiaConfig.php
@@ -2,17 +2,17 @@
 
 namespace Hitech\IndonesiaLaravel\Supports;
 
-readonly class IndonesiaConfig
+class IndonesiaConfig
 {
     public function __construct(
-        public mixed  $pattern,
-        public mixed  $tablePrefix,
-        public string $provinceCode,
-        public string $cityCode,
-        public string $districtCode,
-        public string $villageCode,
-        public string $code,
-        public string $name
+        public readonly mixed  $pattern,
+        public readonly mixed  $tablePrefix,
+        public readonly string $provinceCode,
+        public readonly string $cityCode,
+        public readonly string $districtCode,
+        public readonly string $villageCode,
+        public readonly string $code,
+        public readonly string $name
     )
     {
     }
